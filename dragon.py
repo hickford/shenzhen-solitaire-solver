1d36a575921f75c8bed147cbe674936f2c2ee20c
M Hickford <mirth.hickford@gmail.com>
2017-01-22 11:48:16 +0000

    first moves

--- dragon.py
+++ dragon.py
@@ -20,7 +20,7 @@ def pretty(card, show_empty=False):
     return card.colour[0] + str(card.rank)
 
 Card.__str__ = pretty
-no_card = Card(None, None)
+Card.__bool__ = lambda card: bool(card.colour) or bool(card.rank)
 
 def pretty_row(cards, show_empty=False):
     return " ".join(pretty(card, show_empty) for card in cards)
@@ -29,14 +29,14 @@ def nth(pile, n):
     try:
         return pile[n]
     except IndexError:
-        return None
+        return Card(None, None)
 
 Move = namedtuple('Move', ['cards', 'source', 'destination'])
 
 class Board:
     tableau_locations = [f"pile {i}" for i in range(8)]
     cell_locations = [f"cell {i}" for i in range(3)]
-    foundation_locations = [f"{colour} foundation" for colour in [flower] + colours]
+    foundation_locations = [f"{colour} foundation" for colour in ['flower'] + colours]
 
     def __init__(self):
         self.deal()
@@ -59,15 +59,16 @@ class Board:
         self.moves_explored = 0
 
     def list_legal_moves(self):
-        # from cell to foundation
-        for source, card in zip(cell_locations, self.cells()):
+        # from cell or tableau to foundation
+        for source in self.cell_locations + self.tableau_locations:
+            card = nth(self.piles[source], -1)
             if not card or card.rank == "dragon":
                 continue
 
             destination = f"{card.colour} foundation"
-            self.piles[destination]
-            if card.rank == 1:
-                yield Move([card], source)
+            foundation_card = nth(self.piles[destination], -1)
+            if card.rank == 1 or foundation_card.rank == card.rank - 1:
+                yield Move([card], source, destination)
 
         # from cell to topmost
         # from topmost to cell
@@ -75,20 +76,18 @@ class Board:
         # group from one pile to another
         # four dragons to one cell
 
-        pass
-
     def apply_move(self, move, record=True):
         n = len(move.cards)
-        assert self.piles[source][-n:] == cards
-        self.piles[source] = self.piles[source][:-n]
-        self.piles[destination] = self.piles[destination] + cards
+        assert self.piles[move.source][-n:] == move.cards
+        self.piles[move.source] = self.piles[move.source][:-n]
+        self.piles[move.destination] = self.piles[move.destination] + move.cards
         if record:
             self.move_history.append(move)
 
     def undo(self):
         """Undo most recent move"""
         move = self.move_history.pop()
-        apply_move(Move(move.cards, move.destination, move.source), record=False)
+        self.apply_move(Move(move.cards, move.destination, move.source), record=False)
 
     def solve(self):
         """Solve by backtracking"""
@@ -98,14 +97,19 @@ class Board:
         # how to avoid loops? record past state?
 
         for move in self.list_legal_moves():
+            print(move)
+            self.moves_explored += 1
             self.apply_move(move)
+            print(self)
             success = self.solve()
             if success:
                 return success
+            print("BACKTRACK")
             self.undo()
+            print(board)
 
     def solved(self):
-        return not(any(self.cells)) and not(any(self.tableau))
+        return not(any(self.cells())) and not(any(self.tableau()))
 
     def cells(self):
         """List topmost card or None in each cell"""
@@ -114,12 +118,16 @@ class Board:
         return [nth(pile, -1) for pile in cell_piles]
 
     def foundations(self):
-        """List top most card in each foundation"""
+        """List topmost card in each foundation"""
         return [nth(self.piles[loc], -1) for loc in self.foundation_locations]
 
     def tableau(self):
         """List piles in tableau (list of lists)"""
         return [self.piles[loc] for loc in self.tableau_locations]
+
+    def topmost(self):
+        """List topmost card in each tableau pile"""
+        return [nth(pile, -1) for pile in self.tableau()]
 
     def __str__(self):
         header = pretty_row(self.cells(), show_empty=True) + "    " + pretty_row(self.foundations(), show_empty=True)
@@ -127,7 +135,10 @@ class Board:
         max_height = max(len(pile) for pile in self.tableau())
         tableau = "\n".join(pretty_row(nth(pile, h) for pile in self.tableau()) for h in range(max_height))
 
-        return header + "\n\n" + tableau
+        return header + "\n\n" + tableau + "\n"
 
 board = Board()
 print(board)
+board.solve()
+print()
+print(board.moves_explored)
