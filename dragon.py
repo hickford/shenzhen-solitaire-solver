60fff8a86393afbd8489499ba6cc94c28b8b44a2
M Hickford <mirth.hickford@gmail.com>
2017-01-22 10:04:51 +0000

    consistent data format for piles

--- dragon.py
+++ dragon.py
@@ -1,8 +1,7 @@
 from collections import namedtuple, OrderedDict
 import random
-
 Card = namedtuple('Card', ['colour', 'rank'])
-colours = ('red', 'green', 'black')
+colours = ['red', 'green', 'black']
 
 dragons = [Card(colour, "dragon") for colour in colours for i in range(4)]
 normals = [Card(colour, rank) for colour in colours for rank in range(1,9+1)]
@@ -31,15 +30,29 @@ def nth(pile, n):
     except IndexError:
         return None
 
+Move = namedtuple('Move', ['cards', 'source', 'destination'])
+
 class Board:
+    tableau_locations = [f"pile {i}" for i in range(8)]
+    cell_locations = [f"cell {i}" for i in range(3)]
+    foundation_locations = [f"{colour} foundation" for colour in [flower] + colours]
+
     def __init__(self):
         self.deal()
 
     def deal(self):
+        self.piles = dict()
+        
         random.shuffle(pack)
-        self.tableau = tuple(pack[5*i:5*(i+1)] for i in range(8))
-        self.cells = [None, None, None]
-        self.foundations = OrderedDict((colour, []) for colour in [flower]+list(colours))
+        tableau = [pack[5*i:5*(i+1)] for i in range(8)]
+        for loc, cards in zip(self.tableau_locations, tableau):
+            self.piles[loc] = cards
+
+        for loc in self.cell_locations:
+            self.piles[loc] = []
+
+        for loc in self.foundation_locations:
+            self.piles[loc] = []
 
         self.move_history = list()
         self.moves_explored = 0
@@ -53,8 +66,9 @@ class Board:
 
         pass
 
-    def apply_move():
-        pass
+    def apply_move(self, move):
+        n = len(move.cards)
+
 
     def undo():
         pass
@@ -73,11 +87,25 @@ class Board:
     def solved(self):
         return not(any(self.cells)) and not(any(self.tableau))
 
-    def __str__(self):
-        header = pretty_row(self.cells, show_empty=True) + "    " + pretty_row([nth(foundation, -1) for foundation in self.foundations.values()], show_empty=True)
+    def cells(self):
+        """List card or None in each cell"""
+        cell_piles = [self.piles[loc] for loc in self.cell_locations]
+        assert all(len(pile) <= 1 for pile in cell_piles)
+        return [nth(pile, 0) for pile in cell_piles]
 
-        max_height = max(len(pile) for pile in self.tableau)
-        tableau = "\n".join(pretty_row(nth(pile, h) for pile in self.tableau) for h in range(max_height))
+    def foundations(self):
+        """List top most card in each foundation"""
+        return [nth(self.piles[loc], -1) for loc in self.foundation_locations]
+
+    def tableau(self):
+        """List piles in tableau (list of lists)"""
+        return [self.piles[loc] for loc in self.tableau_locations]
+
+    def __str__(self):
+        header = pretty_row(self.cells(), show_empty=True) + "    " + pretty_row(self.foundations(), show_empty=True)
+
+        max_height = max(len(pile) for pile in self.tableau())
+        tableau = "\n".join(pretty_row(nth(pile, h) for pile in self.tableau()) for h in range(max_height))
 
         return header + "\n\n" + tableau
 
